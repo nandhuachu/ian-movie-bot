11233fb253b020cd02c7bf15bc963058760f1189
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-03-01 21:00:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TOICTONY/ian-movie-bot.git /TheMovieProviderBot
+  git clone https://github.com/TOICTONY/ian-movie-bot.git /ian-movie-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /ian-movie-bot
 fi
 cd /TheMovieProviderBot
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting ian-movie-bot...."
 python3 bot.py
