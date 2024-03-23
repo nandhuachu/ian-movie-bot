1c4032490ca64954477d4f9c8966c4ddb513c7fd
Venom <67759177+nandhuachu@users.noreply.github.com>
2024-03-23 18:23:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TOICTONY/ian-movie-bot.git /ian-movie-bot
+  git clone https://github.com/nandhuachu/ian-movie-bot.git /ian-movie-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /ian-movie-bot
